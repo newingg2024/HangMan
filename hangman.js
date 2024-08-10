4e451fc48a568224d7b00119bb247de9b653441d
nayeem <a.alnayeem2024@gmail.com>
2024-08-11 01:31:12 +0600

    disabled enabled hide show hide

--- hangman.js
+++ hangman.js
@@ -1,3 +1,5 @@
+let challenge = "MANCHESTER";
+
 function GenerateLetters(){
 
     let letterBox =[];
@@ -9,7 +11,6 @@ function GenerateLetters(){
 }
 function GenerateDashes(){
     let dash_line =[];
-    let challenge = "MANCHESTER";
     for(let i=0 ; i<challenge.length;i++){
         console.log(challenge[i]);
         let displayContainerEl = document.getElementById('display');
@@ -26,20 +27,63 @@ function GenerateDashes(){
         let charDiv = document.createElement('div');
         charDiv.id='charDiv';
         charDiv.textContent=challenge[i];
-
+        charDiv.classList='hide';
         letterDiv.appendChild(charDiv);
         letterDiv.appendChild(dashDiv);
 
-
     }
     
-    for(let i=0;i<=8;i++){
+}
 
-        dash_line.push('_');
-        dash_line.textContent= dash_line;
-        console.log(dash_line);
+let lives = 10;
+function DisplayLives(){
+    console.log(lives);
+    let livesEl = document.getElementById('lives');
+    livesEl.textContent= 'lives left: ' + lives;
+
+}
+
+function MatchingAlgo(event){
+    event.target.disabled = true;
+    const user_input= event.target.innerText;
+
+    // if user input available inside challenge
+    // positive case ( show hide)
+    //or else lose life 
+
+    let match = false;
+
+    for(i=0 ; i<challenge.length;i++){
+        console.log(challenge[i]);
+        if(user_input == challenge[i]){
+            match = true;
+            ShowHide(user_input);
+        }
     }
-    return dash_line;
+
+    if (match == false ){                //if(!match){} both are same
+        LoseLife();
+    }
+
+}
+
+function LoseLife(){
+    if( lives > 0){
+        lives = lives - 1;
+    }
+      // lives--
+      if(lives <= 0){
+        lives = 'GAME OVER';
+      }
+    DisplayLives();
+}
+function ShowHide(user_input){
+
+    let searchId = user_input + '_id';
+    const searchEl = document.getElementById(searchId);
+    let charAndDash = searchEl.children;
+    charAndDash[0].classList =[];
+    charAndDash[1].classList= 'hide';
 }
 
 // created buttons with every individual alphabet
@@ -48,6 +92,7 @@ function GenerateButtons(){
     let alphabets = GenerateLetters();
     for(let i=0 ; i < alphabets.length ; i++){
         const NewBtn = document.createElement('button');
+        NewBtn.addEventListener('click',MatchingAlgo);
         NewBtn.textContent= alphabets[i];
         const letterBoxElement = document.getElementById('letter_box');
         letterBoxElement.appendChild(NewBtn);
@@ -58,6 +103,7 @@ function GenerateButtons(){
 function main_flow(){
       GenerateButtons();
       GenerateDashes();
+      DisplayLives();
 }
 
 main_flow();
