fe1548946ffd4a45cd626b6f875132b0f716e0c9
nayeem <a.alnayeem2024@gmail.com>
2024-08-11 00:17:00 +0600

    character and dash div box print

--- hangman.js
+++ hangman.js
@@ -7,6 +7,40 @@ function GenerateLetters(){
     }
     return letterBox;
 }
+function GenerateDashes(){
+    let dash_line =[];
+    let challenge = "MANCHESTER";
+    for(let i=0 ; i<challenge.length;i++){
+        console.log(challenge[i]);
+        let displayContainerEl = document.getElementById('display');
+        let letterDiv =document.createElement('div');
+        
+        letterDiv.id=challenge[i]+'_id';
+        
+        displayContainerEl.appendChild(letterDiv);
+        //creating the dash div
+        let dashDiv =document.createElement('div');
+        dashDiv.id='dashDiv';
+        dashDiv.textContent='_';
+
+        let charDiv = document.createElement('div');
+        charDiv.id='charDiv';
+        charDiv.textContent=challenge[i];
+
+        letterDiv.appendChild(charDiv);
+        letterDiv.appendChild(dashDiv);
+
+
+    }
+    
+    for(let i=0;i<=8;i++){
+
+        dash_line.push('_');
+        dash_line.textContent= dash_line;
+        console.log(dash_line);
+    }
+    return dash_line;
+}
 
 // created buttons with every individual alphabet
 //that we rcvd from GeneratingLetters();
@@ -19,9 +53,11 @@ function GenerateButtons(){
         letterBoxElement.appendChild(NewBtn);
     }
 }
-//to keep the code clean.
-function main_flow(){
-    GenerateButtons();
 
+
+function main_flow(){
+      GenerateButtons();
+      GenerateDashes();
 }
+
 main_flow();
